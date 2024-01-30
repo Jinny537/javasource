7deea02cfe3582d4111f9e049cef0619487966cd
jinny <john051817@gmail.com>
2024-01-30 17:50:35 +0900

    클래스 작성

---
+++ ch1_variable/src/ch1/hello.java
@@ -0,0 +1,11 @@
+package ch1;
+
+// 클래스 이름은 대문자로 시작
+// 클래스명은 한글 가능 하나, 영어로 사용
+public class hello {
+    public static void main(String[] args) {
+        int var1 = 5;
+        System.out.println("내가 좋아하는 숫자는");
+    }
+
+}
